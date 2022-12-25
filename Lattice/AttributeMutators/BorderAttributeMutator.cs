e3cff3227e20066e3f7391bd554833bea82dba72
Ryan Poole <rmp135@gmail.com>
2022-12-25 13:00:14 +0000

    Additional border attributes.

--- Lattice/AttributeMutators/BorderAttributeMutator.cs
+++ Lattice/AttributeMutators/BorderAttributeMutator.cs
@@ -4,39 +4,88 @@ using System.Drawing;
 
 namespace Lattice.AttributeMutators;
 
+public abstract class BaseBorderAttributeMutator : BaseAttributeMutator
+{
+    protected IContainer Mutate(IContainer container, string value, Func<float, Unit, IContainer> func)
+    {
+        return !int.TryParse(value, out var paddingValue) ? container : func(paddingValue, Unit.Point);
+    }
+}
+
 [Export(typeof(IAttributeMutator))]
-public class BorderAttributeMutator : BaseAttributeMutator
+public class BorderAttributeMutator : BaseBorderAttributeMutator
 {
     public override string Name => "border";
 
     protected override IContainer Mutate(IContainer container, string value)
     {
-        var newContainer = container;
-        if (float.TryParse(value, out var border))
-        {
-            newContainer = newContainer.Border(border);
-        }
-        return newContainer;
+        return Mutate(container, value, container.Border);
     }
 }
 
 [Export(typeof(IAttributeMutator))]
-public class BorderColourAttributeMutator : BaseAttributeMutator
+public class BorderLeftAttributeMutator : BaseBorderAttributeMutator
 {
-    private readonly ColourConverter ColourConverter;
-
-    public BorderColourAttributeMutator(ColourConverter colourConverter)
-    {
-        ColourConverter = colourConverter;
-    }
-
-    public override string Name => "borderColour";
+    public override string Name => "borderLeft";
 
     protected override IContainer Mutate(IContainer container, string value)
     {
-        var newContainer = container;
-        var color = ColourConverter.ConvertToHex(value);
-        newContainer = newContainer.BorderColor(color);
-        return newContainer;
+        return Mutate(container, value, container.BorderLeft);
     }
 }
+
+[Export(typeof(IAttributeMutator))]
+public class BorderRightAttributeMutator : BaseBorderAttributeMutator
+{
+    public override string Name => "borderRight";
+
+    protected override IContainer Mutate(IContainer container, string value)
+    {
+        return Mutate(container, value, container.BorderRight);
+    }
+}
+
+[Export(typeof(IAttributeMutator))]
+public class BorderTopAttributeMutator : BaseBorderAttributeMutator
+{
+    public override string Name => "borderTop";
+
+    protected override IContainer Mutate(IContainer container, string value)
+    {
+        return Mutate(container, value, container.BorderTop);
+    }
+}
+
+[Export(typeof(IAttributeMutator))]
+public class BorderBottomAttributeMutator : BaseBorderAttributeMutator
+{
+    public override string Name => "borderBottom";
+
+    protected override IContainer Mutate(IContainer container, string value)
+    {
+        return Mutate(container, value, container.BorderBottom);
+    }
+}
+
+[Export(typeof(IAttributeMutator))]
+public class BorderVerticalAttributeMutator : BaseBorderAttributeMutator
+{
+    public override string Name => "borderVertical";
+
+    protected override IContainer Mutate(IContainer container, string value)
+    {
+        return Mutate(container, value, container.BorderVertical);
+    }
+}
+
+[Export(typeof(IAttributeMutator))]
+public class BorderHorizontalAttributeMutator : BaseBorderAttributeMutator
+{
+    public override string Name => "borderHorizontal";
+
+    protected override IContainer Mutate(IContainer container, string value)
+    {
+        return Mutate(container, value, container.BorderHorizontal);
+    }
+}
+
