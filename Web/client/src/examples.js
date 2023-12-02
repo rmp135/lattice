3c9dfa3e5165c09bb99c6c63abb87e585c91ce29
Ryan Poole <rmp135@gmail.com>
2023-12-02 19:01:58 +0000

    Added basic example.

--- Web/client/src/examples.js
+++
@@ -1,7 +0,0 @@
-﻿import ExampleBlank from './Examples/Blank.xml?raw'
-import GroupingExample from './Examples/Grouping.xml?raw'
-
-export default {
-  "Blank": ExampleBlank,
-  "Grouping and Aggregation": GroupingExample,
-}
---
+++ Web/client/src/examples.ts
@@ -0,0 +1,9 @@
+﻿import ExampleBlank from './examples/Blank.xml?raw' 
+import BasicExample from './examples/Basic.xml?raw' 
+import GroupingExample from './examples/Grouping.xml?raw' 
+
+export default {
+  "Blank": ExampleBlank,
+  "Basic": BasicExample,
+  "Grouping and Aggregation": GroupingExample,
+}
